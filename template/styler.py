c4e364022b693556adf4a50b01b944f8d1b3458a
Nirmal Kumar R <tildezero@gmail.com>
2024-09-18 19:05:50 +0530

    Update styler.py to add args and logic

--- template/styler.py
+++ template/styler.py
@@ -1,15 +1,21 @@
+import argparse
 import pygments
 import pygments.lexers
 from pygments.formatters import HtmlFormatter
 
-code = """\
-func main() {
-    fmt.Println("Hello")
-}
-"""
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Styler syntax highlights the string and convert the output to html formatted file')
+parser.add_argument('value', type=str,
+                    help='A required value to style the given code')
+parser.add_argument('output', type=str,
+                    help='An output path value including filename to save the output of Styler')
+args = parser.parse_args()
+
+code = args.value
 lexer = pygments.lexers.get_lexer_for_filename("main.go")
 formatter = HtmlFormatter(linenos=True, style="xcode", noclasses=True)
 highlighted_text = pygments.highlight(code, lexer, formatter)
 
-with open("out.html", "w") as f:
+with open(args.output, "w") as f:
     f.write(highlighted_text)
